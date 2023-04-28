6cbe95407ef604cf296cbccf98bbaafda167839f
jessica-patel <jessica_patel9@hotmail.com>
2023-04-27 23:32:55 -0400

    Added jQuery functions
    Added a menu toggle function for the hamburger menu icon, and added a smooth scrolling effect when "Contact" is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Hello and welcome to my portfolio site! -Jess")
+
+function menuToggle() { // shows menu links when hamburger is clicked on
+    var x = document.getElementById('myNavtoggle'); // assigns menu object to a variable using the ID (searched document for the provided ID)
+    if (x.className === 'navtoggle') { // checks if classname of x is navtoggle
+      x.className += ' responsive'; // changes class of object based on whether it's shown. this shows menu
+    } else { // aka if it is navtoggle responsive
+      x.className = 'navtoggle'; // effectively hides menu
+    }
+}
+
+function scrollToAnchor(aid){
+    const destination = $("a[name='"+ aid +"']");
+    $('html,body').animate({
+        scrollTop: destination.offset().top
+    },'slow');
+}
+
+$(document).on('click', '.smooth-link', function(){
+    scrollToAnchor('contact');
+});
---
+++ notes.md
@@ -0,0 +1,24 @@
+Questions for Evan
+- Navigation transformation *
+- Cleaning up and organizing *
+- Spacing for headings *
+- Hero section thing *
+- Wrap text around image or make it smaller *
+- finding svg's *
+- How to make project more accessible
+
+
+Evan edits 1.8:
+- Make headings flush with images
+- Add spaces above and below lines
+- Add in real links
+- Row margin
+- “A UX Case Study”
+- Add padding above h1 in index for home page
+- CSS animation is missing
+- Add padding to about
+- Image is tiny in mobile
+
+More questions:
+- not sure why my animation isn't appearing for smallest screen size
+- how can I make it scroll back up from contact section to home page
